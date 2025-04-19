360d6b89fbd71bc9fba6c88ece8cdb968e3c0f02
Mrowr Purr <mrowr.purr@gmail.com>
2025-04-18 18:26:55 -0700

    The DSL I want for now

--- src/PapyrusPad/app/widgets/my_widget.py
+++ src/PapyrusPad/app/widgets/my_widget.py
@@ -6,6 +6,9 @@ from qt_helpers.widget import widget
 
 @widget()
 class MyWidget(QWidget):
-    label1: QLabel = factory(QLabel, "Hi from widget!")
+    label1: QLabel = factory(QLabel)
     label2: QLabel = factory(QLabel, "Another!")
     label3: QLabel = factory(QLabel, "this one too!")
+
+    def setup(self):
+        self.label1.setText("Set in setup!!")
