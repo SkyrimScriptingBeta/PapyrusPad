f61f2a860c52b66e3912f8b01d9e3f8a64beb352
Mrowr Purr <mrowr.purr@gmail.com>
2025-04-18 19:11:28 -0700

    Hmm

--- src/PapyrusPad/app/windows/main_window.py
+++ src/PapyrusPad/app/windows/main_window.py
@@ -1,13 +1,16 @@
+from typing import override
 from PySide6.QtWidgets import QLabel, QMainWindow
 
 from qt_helpers.factory import make
+from qt_helpers.setup_functions_mixins import MainWindowSetupFunctionsMixin
 from qt_helpers.window import window
 
 
 @window("main_window", title="PapyrusPad")
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, MainWindowSetupFunctionsMixin):
     left_widget_example: QLabel = make(QLabel, "Left Widget Example")
     right_widget_example: QLabel = make(QLabel, "Right Widget Example")
 
+    @override
     def setup_layout(self):
         self.add_dock_widget(self.left_widget_example)
