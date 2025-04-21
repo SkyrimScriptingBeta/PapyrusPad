c9255ff774a662d5941e3e1fcf87f4393c2fd0e6
Mrowr Purr <mrowr.purr@gmail.com>
2025-04-21 12:55:07 -0700

    Ok back to DI and DI.foo is what we'll do!

--- src/PapyrusPad/actions/show_about_action.py
+++ src/PapyrusPad/actions/show_about_action.py
@@ -1,7 +1,7 @@
 from typing import override
 from PySide6.QtGui import QAction
-from dependency_injector.wiring import inject
 from PapyrusPad.app.application import Application
+from PapyrusPad.app.dependencies import DI
 from qt_helpers.action import action
 from qt_helpers.interfaces import IAction
 from PySide6.QtWidgets import QMessageBox, QStyle
@@ -11,17 +11,12 @@ from PySide6.QtWidgets import QMessageBox, QStyle
 @action("About", tooltip="Show information about the application", icon=QStyle.StandardPixmap.SP_MessageBoxQuestion)
 class ShowAboutAction(QAction, IAction):
 
-    @inject
     @override
-    def action(self, checked: bool, app: Application | None = None):
-        # app = dependencies.container.application()
+    def action(self, checked: bool, app: Application = DI.application):
         # TODO: make this a widget
         # show a popup with the app name / version
         msg = QMessageBox()
-        if app:
-            msg.setText(f"{app.applicationName()} {app.applicationVersion()}")
-        else:
-            msg.setText("NO APP")
+        msg.setText(f"{app.applicationName()} {app.applicationVersion()}")
         msg.setInformativeText("This is a simple text editor.")
         msg.setWindowTitle("About")
         msg.setStandardButtons(QMessageBox.StandardButton.Ok)
--- src/PapyrusPad/app/dependencies.py
+++ src/PapyrusPad/app/dependencies.py
@@ -1,8 +1,7 @@
 from dependency_injector import containers, providers
-from dependency_injector.wiring import register_loader_containers
+from dependency_injector.wiring import Provide, register_loader_containers
 
 from PapyrusPad.app.application import Application
-from qt_helpers.dependency_injection import DIRegistry
 
 
 # Dependencies
@@ -15,4 +14,6 @@ container = Container()
 container.wire()
 register_loader_containers(container)
 
-DI = DIRegistry(Container)
+
+class DI:
+    application: Application = Provide[Container.application]
