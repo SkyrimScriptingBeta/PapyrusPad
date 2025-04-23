0141f87b05c890235d860ec5230fe559ddba7bfa
Mrowr Purr <mrowr.purr@gmail.com>
2025-04-22 22:22:10 -0700

    TDD (fail)

--- tests/unit/widgets/test_editor_widget.py
+++ tests/unit/widgets/test_editor_widget.py
@@ -19,3 +19,19 @@ class TestEditorWidget:
         # Assert
         assert_that(document.content).is_equal_to("New content")
         assert_that(document.is_modified).is_true()
+
+    def test_document_updates_editor_when_content_changes(self, document_collection: IDocumentCollection):
+        """Test that changes to the document content update the editor text.
+
+        This test is expected to fail with the current implementation because
+        there is no binding from document to editor.
+        """
+        # Arrange
+        document = document_collection.create(name="Test Document", content="")
+        editor = EditorWidget(document=document)
+
+        # Act
+        document.content = "Updated content"
+
+        # Assert - This should fail with the current implementation
+        assert_that(editor.txt_source.toPlainText()).is_equal_to("Updated content")
